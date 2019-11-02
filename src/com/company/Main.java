56c4a6236c5d788945ac48c7c3a91047894d2135
Anna zabolotnikova <annie.space27@gmail.com>
2019-11-02 11:40:25 +0300

    Adding a Mail class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,52 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        try {
+            //Track track = new Track("", "");
+
+            Track track1 = new Track("Through Glass",
+                    "Stone Sour",
+                    "nu metal",
+                    2006);
+            AlbumTrack albumTrack1 = new AlbumTrack("Through Glass",
+                    "Stone Sour",
+                    "nu metal",
+                    2006,
+                    "Come What(ever) May10th Anniversary Edition");
+
+            AlbumTrack albumTrack2 = new AlbumTrack("Another Home",
+                    "Koven",
+                    "dabstep",
+                    2014,
+                    "Another Home");
+
+            AlbumTrack albumTrack3 = new AlbumTrack("Once",
+                    "Khamsin",
+                    "electronics",
+                    2016,
+                    "Leaving");
+
+            Catalog catalog = new Catalog();
+
+            catalog.add(albumTrack1);
+            catalog.add(albumTrack2);
+            catalog.add(albumTrack3);
+
+            catalog.sortByName();
+            System.out.println(catalog.toString());
+
+            catalog.sortByAuthor();
+            System.out.println(catalog.toString());
+
+            System.out.println(catalog.findByName("Through Glass"));
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
